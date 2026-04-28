e9ba536c6a3d8c3fc706d5f7f4b412bf97eb7799
ve312 <felipea201903@gmail.com>
2026-04-28 07:21:32 -0500

    feat(domain): agregar modelos de medición, predicción y estado de planta

---
+++ src/main/java/com/proyectosu/invernadero/domain/model/EstadoPlanta.java
@@ -0,0 +1,24 @@
+package com.proyectosu.invernadero.domain.model;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+public class EstadoPlanta {
+    private UUID id;
+    private Dispositivo dispositivo;
+    private LocalDateTime fechaHora;
+    private String estado;
+
+    public EstadoPlanta(UUID id,
+                        Dispositivo dispositivo,
+                        LocalDateTime fechaHora,
+                        String estado) {
+        this.id = id;
+        this.dispositivo = dispositivo;
+        this.fechaHora = fechaHora;
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/proyectosu/invernadero/domain/model/MedicionSensor.java
@@ -0,0 +1,36 @@
+package com.proyectosu.invernadero.domain.model;
+
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+public class MedicionSensor {
+
+    private UUID id;
+    private Dispositivo dispositivo;
+    private LocalDateTime fechaHora;
+
+    private BigDecimal temperatura;
+    private BigDecimal humedadSuelo;
+    private BigDecimal humedadRelativa;
+    private BigDecimal luz;
+
+    public MedicionSensor(UUID id,
+                          Dispositivo dispositivo,
+                          LocalDateTime fechaHora,
+                          BigDecimal temperatura,
+                          BigDecimal humedadSuelo,
+                          BigDecimal humedadRelativa,
+                          BigDecimal luz) {
+        this.id = id;
+        this.dispositivo = dispositivo;
+        this.fechaHora = fechaHora;
+        this.temperatura = temperatura;
+        this.humedadSuelo = humedadSuelo;
+        this.humedadRelativa = humedadRelativa;
+        this.luz = luz;
+    }
+}
