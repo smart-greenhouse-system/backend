ea96489da03c9f1d070119b62b2508c2a023f85e
ve312 <felipea201903@gmail.com>
2026-04-28 07:15:34 -0500

    feat: crear endpoints y DTOs de autenticación

---
+++ src/main/java/com/proyectosu/invernadero/rest/dto/request/RegisterRequest.java
@@ -0,0 +1,18 @@
+package com.proyectosu.invernadero.rest.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class RegisterRequest {
+
+    @Email
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    @Size(min = 8)
+    private String password;
+}
