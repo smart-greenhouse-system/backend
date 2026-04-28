b8a81ad5e78d21de4b3dd9c88dc5e5c34eafdb4c
ve312 <felipea201903@gmail.com>
2026-04-28 07:08:49 -0500

    feat: crear entidades JPA de usuarios y roles

---
+++ src/main/java/com/proyectosu/invernadero/infraestructure/entity/RoleEntity.java
@@ -0,0 +1,20 @@
+package com.proyectosu.invernadero.infraestructure.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public RoleEntity() {}
+}
