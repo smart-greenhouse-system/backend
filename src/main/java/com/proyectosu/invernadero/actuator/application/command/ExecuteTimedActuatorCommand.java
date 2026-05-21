f7d02812fab4742e51cd3f7c4a3d59772e9402b8
ve312 <felipea201903@gmail.com>
2026-05-20 20:08:08 -0500

    feat(api-contracts): add automation commands and dto contracts

--- src/main/java/com/proyectosu/invernadero/actuator/application/command/ExecuteTimedActuatorCommand.java
+++ src/main/java/com/proyectosu/invernadero/actuator/application/command/ExecuteTimedActuatorCommand.java
@@ -1,4 +1,12 @@
 package com.proyectosu.invernadero.actuator.application.command;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class ExecuteTimedActuatorCommand {
+    private final String actuatorId;
+    private final Integer timeActionSeconds;
+    private final String origin;
 }
--- src/main/java/com/proyectosu/invernadero/actuator/dto/response/ExecuteActuatorResponse.java
+++ src/main/java/com/proyectosu/invernadero/actuator/dto/response/ExecuteActuatorResponse.java
@@ -1,4 +1,14 @@
 package com.proyectosu.invernadero.actuator.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class ExecuteActuatorResponse {
+    private String message;
+    private String deviceId;
+    private String actuator;
+    private String action;
+    private Boolean executed;
 }
