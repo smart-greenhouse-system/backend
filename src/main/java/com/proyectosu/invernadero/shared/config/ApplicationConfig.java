d7b46069c9c47ca5b3744a827a1a58307b85f6c2
ve312 <felipea201903@gmail.com>
2026-05-20 20:09:40 -0500

    feat(config): register automation usecases and scheduler

--- src/main/java/com/proyectosu/invernadero/shared/config/ApplicationConfig.java
+++ src/main/java/com/proyectosu/invernadero/shared/config/ApplicationConfig.java
@@ -1,13 +1,22 @@
 package com.proyectosu.invernadero.shared.config;
 
+import com.proyectosu.invernadero.actuator.application.usecases.ExecuteTimedActuatorUseCase;
+import com.proyectosu.invernadero.actuator.domain.port.ActuatorEventRepositoryPort;
+import com.proyectosu.invernadero.actuator.domain.port.ActuatorResolverPort;
 import com.proyectosu.invernadero.evento.application.usecase.GuardarEventoUseCase;
 import com.proyectosu.invernadero.auth.application.usecase.LoginUserUseCase;
 import com.proyectosu.invernadero.evento.application.usecase.ObtenerEventosUseCase;
 import com.proyectosu.invernadero.auth.application.usecase.RegisterUserUseCase;
 import com.proyectosu.invernadero.auth.domain.ports.*;
 import com.proyectosu.invernadero.evento.domain.ports.EventoRepositoryPort;
+import com.proyectosu.invernadero.prediction.application.usecases.ProcessAiPredictionUseCase;
+import com.proyectosu.invernadero.prediction.domain.port.GreenhouseConfigReaderPort;
+import com.proyectosu.invernadero.prediction.domain.port.PredictionRepositoryPort;
+import com.proyectosu.invernadero.prediction.domain.port.TimedActuatorExecutorPort;
+import com.proyectosu.invernadero.shared.domain.ports.MqttPublisherPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 
 @Configuration
 public class ApplicationConfig {
@@ -43,4 +52,34 @@ public class ApplicationConfig {
     public ObtenerEventosUseCase obtenerEventosUseCase(EventoRepositoryPort eventoRepositoryPort) {
         return new ObtenerEventosUseCase(eventoRepositoryPort);
     }
+
+
+    @Bean
+    public ProcessAiPredictionUseCase processAiPredictionUseCase(
+            PredictionRepositoryPort predictionRepositoryPort,
+            GreenhouseConfigReaderPort greenhouseConfigReaderPort,
+            TimedActuatorExecutorPort timedActuatorExecutorPort
+    ) {
+        return new ProcessAiPredictionUseCase(
+                predictionRepositoryPort,
+                greenhouseConfigReaderPort,
+                timedActuatorExecutorPort
+        );
+    }
+
+    @Bean
+    public TimedActuatorExecutorPort timedActuatorExecutorPort(
+            ActuatorResolverPort actuatorResolverPort,
+            ActuatorEventRepositoryPort actuatorEventRepositoryPort,
+            MqttPublisherPort mqttPublisherPort,
+            TaskScheduler taskScheduler
+    ) {
+        return new ExecuteTimedActuatorUseCase(
+                actuatorResolverPort,
+                actuatorEventRepositoryPort,
+                mqttPublisherPort,
+                taskScheduler
+        );
+    }
+
 }
